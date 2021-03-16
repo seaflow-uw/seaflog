2b4685675d5b862ebe99ad6bdfc141754fc74f05
Chris Berthiaume <chrisbee@uw.edu>
2021-03-16 12:29:12 -0700

    Gzip build products

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@
 VERSION=$(git describe --long --dirty --tags)
 GOOS=darwin GOARCH=amd64 go build -o "seaflog-${VERSION}-darwin-amd64" cmd/seaflog/main.go || exit 1
 GOOS=linux GOARCH=amd64 go build -o "seaflog-${VERSION}-linux-amd64" cmd/seaflog/main.go || exit 1
+gzip "seaflog-${VERSION}-darwin-amd64"
+gzip "seaflog-${VERSION}-linux-amd64"
