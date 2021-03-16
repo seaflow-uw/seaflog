3ab60b6de741528df22680ca0ea42de6ee1080de
Chris Berthiaume <chrisbee@uw.edu>
2021-03-16 12:25:38 -0700

    Stop using zip files for builds

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@
 # Build seaflog command-line tool for 64-bit MacOS and Linux
 
 VERSION=$(git describe --long --dirty --tags)
-GOOS=darwin GOARCH=amd64 go build -o "seaflog.${VERSION}.darwin-amd64/seaflog" cmd/seaflog/main.go || exit 1
-GOOS=linux GOARCH=amd64 go build -o "seaflog.${VERSION}.linux-amd64/seaflog" cmd/seaflog/main.go || exit 1
-zip -q -r "seaflog.${VERSION}.darwin-amd64.zip" "seaflog.${VERSION}.darwin-amd64" || exit 1
-zip -q -r "seaflog.${VERSION}.linux-amd64.zip" "seaflog.${VERSION}.linux-amd64"|| exit 1
+GOOS=darwin GOARCH=amd64 go build -o "seaflog-${VERSION}-darwin-amd64" cmd/seaflog/main.go || exit 1
+GOOS=linux GOARCH=amd64 go build -o "seaflog-${VERSION}-linux-amd64" cmd/seaflog/main.go || exit 1
